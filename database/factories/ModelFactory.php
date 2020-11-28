d0e1d9ae05d6660f4750f7b02132b502a6e812a9
Bernd Engels <engels@goldenacker.de>
2020-11-28 02:21:11 +0100

    initial commit

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var  Factory $factory */
+
+use Illuminate\Database\Eloquent\Factory;
+
+$factory->define(Brackets\AdminAuth\Models\AdminUser::class, function (Faker\Generator $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->email,
+        'password' => bcrypt($faker->password),
+        'remember_token' => null,
+        'activated' => true,
+        'forbidden' => $faker->boolean(),
+        'language' => 'en',
+        'deleted_at' => null,
+        'created_at' => $faker->dateTime,
+        'updated_at' => $faker->dateTime,
+        'last_login_at' => $faker->dateTime,
+
+    ];
+});
---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+const mix = require('laravel-mix');
+/*
+mix
+    .js('resources/js/app.js', 'public/js')
+    .js('resources/js/admin/admin.js', 'public/js')
+    .sass('resources/sass/admin/admin.scss', 'public/css')
+    .copy('resources/css/app.css', 'public/css')
+;
+*/
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
+
+mix.js(['resources/js/admin/admin.js'], 'public/js')
+    .sass('resources/sass/admin/admin.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
