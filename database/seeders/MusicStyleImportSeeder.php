78b8b7fa291d383b068a797bb64da19fdb889580
Bernd Engels <engels@goldenacker.de>
2020-11-29 14:34:27 +0100

    add import seeder

---
+++ database/seeders/MusicStyleImportSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MusicStyleImportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
