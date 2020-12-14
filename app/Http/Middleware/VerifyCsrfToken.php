94c26b8edec987f54719507b654b1da5c6bb3d57
Bernd Engels <engels@f50.de>
2020-12-14 06:37:52 +0100

    bugfix

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'stripe/*',
-        'webhook/paypal/*',
-        'webhook/stripe/*',
+        'payment/*/webhook',
     ];
 }
