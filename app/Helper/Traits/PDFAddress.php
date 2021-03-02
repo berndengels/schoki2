011f3646aadcd71f3bcce7185cb1a02fec6f89af
berndengels <engels@f50.de>
2021-03-02 19:12:46 +0100

    pdf, stuff

--- app/Helper/Traits/PDFAddress.php
+++ app/Helper/Traits/PDFAddress.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Helper\Traits;
 
-use PDF;
 use App\Models\Shipping;
+use Barryvdh\DomPDF\PDF;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\App;
 
 trait PDFAddress
 {
@@ -11,7 +12,8 @@ trait PDFAddress
         /**
          * @var $pdf PDF
          */
-        $pdf = PDF::loadView('admin.address.pdf', compact('shipping'));
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadView('admin.address.pdf', compact('shipping'));
         $fileName = Str::kebab($shipping->customer->name) . '.pdf';
         return $pdf->download($fileName);
     }
